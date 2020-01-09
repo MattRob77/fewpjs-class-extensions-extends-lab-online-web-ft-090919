9c9f606863ee5c9b13c3307cf17157b7345e3a70
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-09 13:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ class Polygon {
   }
 }
 
-class Triangle extends Polygon() {
-  get isValid() {
-    let a = this.array[0]
-    let b = this.array[1]
-    let c = this.array[2]
-    if ( a + b > c && a + c > b && b + c > a ) { 
-    return true 
-    } 
-    return false 
-  }
+class Triangle extends Polygon {
+    get isValid() {
+       let a = this.array[0]
+       let b = this.array[1]
+       let c = this.array[2]
+       if ( a + b > c && a + c > b && b + c > a ) {
+           return true 
+       }
+       return false 
+    }
 }
